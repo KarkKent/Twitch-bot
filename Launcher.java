777817a4202beaf50e0fe5ccbe70594faa84a626
Anton Kulbakov <culbackov@yandex.ru>
2024-08-07 21:02:36 +0200

    test

--- Launcher.java
+++ Launcher.java
@@ -3,7 +3,7 @@ public class Launcher {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("I'm future bot");
+		System.out.println("I'm future");
 
 	}
 
