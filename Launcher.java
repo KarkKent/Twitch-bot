7b25ff93134456d82eeed8936f1efff661dea5da
Anton Kulbakov <culbackov@yandex.ru>
2024-08-09 10:06:37 +0200

    First commands with switch

--- Launcher.java
+++ Launcher.java
@@ -1,10 +1,22 @@
+import java.util.Scanner;
 
 public class Launcher {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("I'm future");
+		Scanner input = new Scanner(System.in);
+		String commandLine=input.next();
+		switch(commandLine) {
+		case "!pc":
+			System.out.println("My pc configuration");
+			break;
+		default:
+			System.out.println("There is no such command");
+			break;
+		}
 
+		
 	}
 
 }
